771b6d75f551f324e89fb9eb6c5847595f408e9f
Shipan Ahmed <shipan.cse.brur@gmail.com>
2026-02-24 04:09:23 +0600

    updated traggle function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let currentFilter = "all";
+
 let jobCards = [
   {
     id: 1,
@@ -8,7 +10,7 @@ let jobCards = [
     salary: "৳60,000 – ৳90,000 / month",
     description:
       "Develop and maintain scalable backend services using Node.js and PostgreSQL. Collaborate with frontend teams to build RESTful APIs and optimize performance for high-traffic applications.",
-    status: "active",
+    status: "all",
   },
   {
     id: 2,
@@ -19,7 +21,7 @@ let jobCards = [
     salary: "৳45,000 – ৳70,000 / month",
     description:
       "Work with a cross-functional team to build enterprise-level applications using .NET and SQL Server. Participate in code reviews and follow software development best practices.",
-    status: "active",
+    status: "all",
   },
   {
     id: 3,
@@ -30,7 +32,7 @@ let jobCards = [
     salary: "৳70,000 – ৳110,000 / month",
     description:
       "Design and develop full-stack web applications using React and Java Spring Boot. Focus on clean architecture, scalability, and performance optimization.",
-    status: "active",
+    status: "all",
   },
   {
     id: 4,
@@ -41,7 +43,7 @@ let jobCards = [
     salary: "৳55,000 – ৳85,000 / month",
     description:
       "Build and maintain Java-based enterprise systems. Work with international clients and follow agile methodologies for efficient software delivery.",
-    status: "active",
+    status: "all",
   },
   {
     id: 5,
@@ -52,7 +54,7 @@ let jobCards = [
     salary: "৳15,000 – ৳25,000 / month",
     description:
       "Assist in building responsive UI components using React.js. Collaborate with senior developers to improve user experience and application performance.",
-    status: "active",
+    status: "all",
   },
   {
     id: 6,
@@ -63,7 +65,7 @@ let jobCards = [
     salary: "৳80,000 – ৳130,000 / month",
     description:
       "Develop machine learning models and integrate them into production systems. Work with large datasets and implement data processing pipelines.",
-    status: "active",
+    status: "all",
   },
   {
     id: 7,
@@ -74,7 +76,7 @@ let jobCards = [
     salary: "৳100,000 – ৳160,000 / month",
     description:
       "Build scalable web applications for European clients using modern JavaScript frameworks. Ensure high code quality and participate in international agile teams.",
-    status: "active",
+    status: "all",
   },
   {
     id: 8,
@@ -85,10 +87,9 @@ let jobCards = [
     salary: "৳50,000 – ৳75,000 / month",
     description:
       "Design, optimize, and maintain relational databases. Write complex queries, improve indexing strategies, and ensure data integrity and security.",
-    status: "active",
+    status: "all",
   },
 ];
-
 
 // update Deshboard
 
@@ -96,35 +97,93 @@ function updateDashboard() {
   let totalJobs = jobCards.length;
   document.getElementById("total").innerText = totalJobs;
 
-  let interviewJobs = jobCards.filter(j => j.status === "INTERVIEW").length;
+  let interviewJobs = jobCards.filter((j) => j.status === "interview").length;
   document.getElementById("interview").innerText = interviewJobs;
 
-  let rejectedJobs = jobCards.filter(j => j.status === "REJECTED").length;
+  let rejectedJobs = jobCards.filter((j) => j.status === "rejected").length;
   document.getElementById("rejected").innerText = rejectedJobs;
 }
 
+// renderFunction
 
+function renderJobs() {
+  let jobContainer = document.getElementById("jobContainer");
+  jobContainer.innerHTML = "";
 
+  let filteredJobs =
+    currentFilter === "all"
+      ? jobCards
+      : jobCards.filter((j) => j.status === currentFilter);
 
-// console.log(jobCards);
+  document.getElementById("tabCount").innerText = filteredJobs.length;
 
-function btnClick(id) {
-  let allBtn = document.querySelectorAll(".btn");
-  for (let btn of allBtn) {
-    btn.classList.remove("bg-blue-500", "text-white");
-    btn.classList.add("bg-gray-200", "text-black");
+  if (filteredJobs.length === 0) {
+    document.getElementById("emptyState").classList.remove("hidden");
+  } else {
+    document.getElementById("emptyState").classList.add("hidden");
   }
-  document.getElementById(id).classList.remove("bg-gray-200", "text-black");
-  document.getElementById(id).classList.add("bg-blue-700", "text-white");
-  
+
+  filteredJobs.forEach((job) => {
+    let jobCard = document.createElement("div");
+    jobCard.classList.add("bg-white", "p-5", "rounded-lg", "shadow-md");
+    jobCard.innerHTML = `
+    
+    <button onclick="deleteJob(${job.id})" class=" absolute top-1 right-1"><img class="w-[20px] h-[20px]" src="./imges/delete-btn.png" alt=""></button>
+    <h2 class="text-blue-700 font-bold">${job.companyName}</h2>
+            <p class="text-black font-medium ">${job.position}</p>
+            <p class="text-gray-600">${job.location}</p>
+            <p class="space-x-5 mt-4 mb-4"><span class="text-gray-600">${job.type}</span><span class="text-gray-600">•৳${job.salary}</span> </p>
+            <span id="status" class="bg-gray-200  px-2 py-1 rounded-md">${job.status.toUpperCase()}</span>
+            <p class="mt-4 text-gray-600">${job.description}</p>
+            <button onclick="traggleStatus(${job.id}, 'INTERVIEW')" id="INTERVIEW" class="mt-4 bg-green-200 text-green-700 px-4 py-2 rounded-lg">INTERVIEW</button>
+            <button onclick="traggleStatus(${job.id}, 'REJECTED')" id="REJECTED" class="mt-4 bg-red-200 text-red-700 px-4 py-2 rounded-lg">REJECTED</button>
+    `;
+    jobContainer.appendChild(jobCard);
+  });
 }
 
+// delete section
+function deleteJob(id) {
+  jobCards = jobCards.filter((job) => job.id !== id);
+  updateDashboard();
+  renderJobs();
+}
+
+// function btnClick(id) {
+//   let allBtn = document.querySelectorAll(".btn");
+//   for (let btn of allBtn) {
+//     btn.classList.remove("bg-blue-500", "text-white");
+//     btn.classList.add("bg-gray-200", "text-black");
+//   }
+//   document.getElementById(id).classList.remove("bg-gray-200", "text-black");
+//   document.getElementById(id).classList.add("bg-blue-700", "text-white");
+//   currentFilter = id;
+//   renderJobs();
+// }
+
 function traggleStatus(id, newStatus) {
-  let job = jobCards.find(j => j.id === id);
+  let job = jobCards.find((j) => j.id === id);
   if (job.status === newStatus) {
-    job.status = "ALL";
+    job.status = "all";
   } else {
     job.status = newStatus;
   }
   updateDashboard();
+  renderJobs();
 }
+
+
+ document.querySelectorAll(".btn").forEach((btn) => {
+   btn.addEventListener("click", () => {
+     document.querySelectorAll(".btn").forEach((b) => {
+       b.classList.remove("bg-blue-600", "text-white");
+       b.classList.add("bg-gray-200");
+     });
+
+     btn.classList.add("bg-blue-600", "text-white");
+     btn.classList.remove("bg-gray-200");
+
+     currentTab = btn.dataset.tab;
+     renderJobs();
+   });
+ });
